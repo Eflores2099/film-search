57d2151310421c9bdbbfd232003345cc6cd7f145
Erik Flores <erik.flores@colorado.edu>
2020-05-21 22:53:07 -0600

    hmr error

--- src/SearchFilm.js
+++ src/SearchFilm.js
@@ -2,9 +2,27 @@ import React from 'react'
 
 export default function SearchFilms() {
 
+    const searchFilms = async (e) => {
+        e.preventDefault()
+        console.log("submitting")
+
+        const query = "Jurassic Park"
+
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=c5d507f4560f331eca8c1fc7672be73c&language=en-US&query=${query}&page=1&include_adult=false`
+
+        try {
+
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+        }catch(err) {
+            console.error(err)
+        }
+    }
+
     return (
-        <form class = "form">
-            <label  htmlFor ='query' className = 'label'>Film Name</label>
+        <form className = "form" onSubmit= {SearchFilms}>
+            <label className = 'label' htmlFor ='query' >Film Name</label>
             <input type= 'text' className = 'input' placeholder ='i.e. The Godfather' ></input>
             <button className ='btn' type ='submit'>Search</button>
         </form>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchFilms from './searchFilms'
+import SearchFilms from './SearchFilm.js'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class App extends React.Component {
+class Main extends React.Component {
   render() {
     return (
       <div className="container">
@@ -16,12 +15,7 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
